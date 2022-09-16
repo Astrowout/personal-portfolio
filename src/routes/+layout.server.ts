2a2c01c155fc30a21ec25aa0118b778e1599103c
Wout Vandesompele <33844638+wowtvds@users.noreply.github.com>
2022-09-16 18:31:40 +0200

    fix: use auto adapter

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,6 +2,8 @@ import { error } from '@sveltejs/kit';
 import query from "$lib/apollo";
 
 import GLOBAL_QUERY from "../queries/globals";
+
+export const prerender = true;
  
 export async function load() {
     const { data } = await query(GLOBAL_QUERY);
